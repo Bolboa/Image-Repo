bfd1b40f0458b2771f555f6fe5b75baaeedfecf0
eric.bolboa <eric.bolboa@verafin.com>
2020-08-30 16:21:19 -0400

    Multipart upload for single file.

--- upload.js
+++ upload.js
@@ -7,56 +7,65 @@ const PART_SIZE = 1024 * 1024 * 5;
 const MAX_UPLOAD_RETRIES = 3;
 const BUCKET = process.env.BUCKET_NAME;
 
-var multiPartParams = {
-  Bucket: BUCKET,
-  Key: "test",
-  ContentType: 'image/jpeg'
-};
-
 var multipartMap = {
   Parts: []
 };
 
 const uploadFile = async (ctx) => {
   const s3 = new AWS.S3();
-  var buffer = fs.readFileSync(ctx.request.files.test.path);
-  let partNum = 0;
-  s3.createMultipartUpload(multiPartParams, (err, multipart) => {
+  const buffer = fs.readFileSync(ctx.request.files.test.path);
+  const fileName = ctx.request.files.test.name;
+  let sizeLeft = Math.ceil(buffer.length / PART_SIZE);
+
+  s3.createMultipartUpload({
+    Bucket: BUCKET,
+    Key: fileName,
+    ContentType: 'application/pdf'
+  }, (err, multipart) => {
     if (err) console.log(err, err.stack);
-    else console.log(multipart);
-    
-    for (let rangeStart = 0; rangeStart < buffer.size; rangeStart += PART_SIZE) {
+
+    let partNum = 0;
+    for (let rangeStart = 0; rangeStart < buffer.length; rangeStart += PART_SIZE) {
       partNum++;
       const partParams = {
         Body: buffer.slice(rangeStart, Math.min(rangeStart + PART_SIZE, buffer.length)),
         Bucket: BUCKET,
-        Key: "test",
+        Key: fileName,
         PartNumber: String(partNum),
         UploadId: multipart.UploadId
       };
-      uploadSlice(s3, multipart, partParams);
+      uploadSlice(s3, partParams, --sizeLeft);
     }
   });
 }
 
-const uploadSlice = (s3, multiPart, partParams, retries=1) => {
-  s3.uploadPart(partParams, (err, data) => {
+const uploadSlice = async (s3, partParams, sizeLeft, retries=1) => {
+  await s3.uploadPart(partParams, (err, data) => {
+    const partNum = partParams.PartNumber;
     if (err) {
-      if (retries < MAX_UPLOAD_RETRIES) {
-        uploadSlice(s3, multipart, partParams, retries+1);
-      }
-      else {
-        console.log('Failed uploading part: #', partParams.PartNumber)
-      }
+      if (retries < MAX_UPLOAD_RETRIES)
+        uploadSlice(s3, partParams, retries+1);
+      else
+        console.log('Failed uploading part: #', partNum)
       return;
     }
-    const partNum = partParams.PartNumber;
+
     multipartMap.Parts[partNum - 1] = {
       ETag: data.ETag,
       PartNumber: Number(partNum)
     }
-    console.log(multipartMap);
 
+    if (sizeLeft <= 0) {
+      s3.completeMultipartUpload({
+        Bucket: BUCKET,
+        Key: partParams.Key,
+        MultipartUpload: multipartMap,
+        UploadId: partParams.UploadId
+      }, (err, data) => {
+        if (err) console.log(err, err.stack);
+        else console.log('Upload Successful', data);
+      });
+    }
   });
 }
 
