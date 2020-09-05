68aa169c3e78a6680239d080362a581087ae4bb4
eric.bolboa <eric.bolboa@verafin.com>
2020-09-05 12:19:49 -0400

    Unit tests for zip upload.

--- app/routes/test/destination.js
+++ app/routes/test/destination.js
@@ -5,15 +5,9 @@ AWS.config.update({ accessKeyId: process.env.ID, secretAccessKey: process.env.SE
 const s3 = new AWS.S3();
 
 const NUMBER_OF_IMAGE_TYPES = 4;
-const NUMBER_OF_ZIP_TYPES = 1;
 
 const testParamsForImageBucket = {
 	Bucket: process.env.IMAGE_BUCKET_NAME,
-	Prefix: 'test/'
-};
-
-const testParamsForZipBucket = {
-	Bucket: process.env.ZIP_BUCKET_NAME,
 	Prefix: 'test/'
 };
 
@@ -28,24 +22,29 @@ const destinationTest = () => describe('Destination Content', () => {
 	test.each(cases)('Check Image Type %p', 
 	async (type) => {
 		const imageData = await testContent(testParamsForImageBucket);
-		const result = await containsType(imageData.Contents, type);
+		const imageDataContent = imageData.Contents.filter(data => { return !data.Key.includes('zip') });
+		const result = await containsType(imageDataContent, type);
+		expect(result).toBe(true);
+	});
+
+	test.each(cases)('Check Image Type From Zip %p', 
+	async (type) => {
+		const imageData = await testContent(testParamsForImageBucket);
+		const imageDataContent = imageData.Contents.filter(data => { return data.Key.includes('zip') });
+		const result = await containsType(imageDataContent, type);
 		expect(result).toBe(true);
 	});
 
 	test('Check If Images Were Uploaded', async () => {
 		const imageData = await testContent(testParamsForImageBucket);
-		await expect(imageData.KeyCount).toBe(NUMBER_OF_IMAGE_TYPES);
+		const imageDataContent = imageData.Contents.filter(data => { return !data.Key.includes('zip') });
+		await expect(imageDataContent.length).toBe(NUMBER_OF_IMAGE_TYPES);
 	});
 
-	test('Check Zip Types', async () => {
-		const zipData = await testContent(testParamsForZipBucket);
-		const result = await containsType(zipData.Contents, 'zip');
-		expect(result).toBe(true);
-	});
-
-	test('Check If Zip Was Uploaded', async () => {
-		const zipData = await testContent(testParamsForZipBucket);
-		await expect(zipData.KeyCount).toBe(NUMBER_OF_ZIP_TYPES);
+	test('Check If Image Uploaded Using Zip', async () => {
+		const imageData = await testContent(testParamsForImageBucket);
+		const imageDataContent = imageData.Contents.filter(data => { return data.Key.includes('zip') });
+		await expect(imageDataContent.length).toBe(NUMBER_OF_IMAGE_TYPES);
 	});
 });
 
